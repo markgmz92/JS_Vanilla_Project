e55b7ac998fe977b893ce4fb75468934a901ca1c
markgmz92 <markgmz92@gmail.com>
2020-03-22 02:49:39 +0800

    Selecting movie seats from UI

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -4,15 +4,31 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
+// Update total and count
+function updateSelectCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
 
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectCount();
+});
+
+//Seat click event
 container.addEventListener('click', e => {
     if (
-        e.target.classList.contains('seat') &&!e.target.classList.contains('occupied')
-        )
-            {
-                e.target.classList.toggle('selected');
-            }
+        e.target.classList.contains('seat') && 
+        !e.target.classList.contains('occupied')
+    ){
+        e.target.classList.toggle('selected');
+
+        updateSelectCount();
     }
-)
+});
