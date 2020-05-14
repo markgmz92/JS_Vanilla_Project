4921a1463cd8fa63f2eb2e2085ccb3923ee4c1d2
markgmz92 <markgmz92@gmail.com>
2020-05-14 23:08:43 +0800

    Letter Press Event Handler

--- Hangman/script.js
+++ Hangman/script.js
@@ -35,5 +35,45 @@ function displayWord() {
    }
 }
 
+//Update the wrong letters
+function updateWrongLettersEl(){
+    console.log('update wrong');
+}
+
+//Show notification
+
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 5000);
+}
+
+//Keydown letter press
+window.addEventListener('keydown', e => {
+    //console.log(e.keycode);
+    if(e.keyCode >=65 && e.keyCode <=90) {
+        const letter = e.key;
+
+        if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter);
+
+                displayWord();
+            } else {
+                showNotification();
+            }
+        } else {
+            if(!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+
+                updateWrongLettersEl();
+            } else {
+                showNotification();
+            }
+        }
+    }
+})
 
 displayWord();
